2d306298083dfe1c0ddf536fd512c4d506dee28c
armbareum-beep <armbareum@example.com>
2025-12-18 12:48:58 +0900

    강좌 카드 그리드 레이아웃 개선 (4열3열로 변경하여 카드 크기 증가)

--- pages/Browse.tsx
+++ pages/Browse.tsx
@@ -146,7 +146,7 @@ export const Browse: React.FC = () => {
           </div>
 
           {filteredCourses.length > 0 ? (
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+            <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6">
               {filteredCourses.map((course) => (
                 <CourseCard key={course.id} course={course} />
               ))}
