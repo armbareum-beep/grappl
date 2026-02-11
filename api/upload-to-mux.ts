f317702d140a978b603745fdf4159267b6305465
armbareum-beep <armbareum@example.com>
2026-02-11 20:32:19 +0900

    fix: allow all CORS origins for Mux upload

--- api/upload-to-mux.ts
+++ api/upload-to-mux.ts
@@ -65,7 +65,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
                     'Content-Type': 'application/json'
                 },
                 body: JSON.stringify({
-                    cors_origin: 'https://grapplay.com',
+                    cors_origin: '*',
                     new_asset_settings: {
                         playback_policy: ['public']
                     }
